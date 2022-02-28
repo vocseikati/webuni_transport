e6924ccbc1ca325cf80d2d04371ab1ae3f3cbe68
Katka <vocseikati@gmail.com>
2022-02-28 14:09:25 +0100

    tests fixed

--- src/main/java/hu/katka/transport/dtos/LoginDto.java
+++ src/main/java/hu/katka/transport/dtos/LoginDto.java
@@ -1,8 +1,10 @@
 package hu.katka.transport.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginDto {
 
   private String username;
--- src/test/java/hu/katka/transport/controllers/TransportPlanControllerTest.java
+++ src/test/java/hu/katka/transport/controllers/TransportPlanControllerTest.java
@@ -1,10 +1,10 @@
 package hu.katka.transport.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import hu.katka.transport.configurations.TransportConfigurationProperties;
 import hu.katka.transport.dtos.DelayDto;
-import hu.katka.transport.entities.Address;
+import hu.katka.transport.dtos.LoginDto;
 import hu.katka.transport.entities.Milestone;
 import hu.katka.transport.entities.Section;
 import hu.katka.transport.entities.TransportPlan;
@@ -13,20 +13,20 @@ import hu.katka.transport.repositories.MilestoneRepository;
 import hu.katka.transport.repositories.SectionRepository;
 import hu.katka.transport.repositories.TransportPlanRepository;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
 class TransportPlanControllerTest {
 
   public static final String BASE_URI = "/api/transportPlans";
+
+  private String jwt;
 
   @Autowired
   WebTestClient webTestClient;
@@ -43,44 +43,51 @@ class TransportPlanControllerTest {
   @Autowired
   SectionRepository sectionRepository;
 
+  @Autowired
+  TransportConfigurationProperties config;
+
   @BeforeEach
   public void init() {
-    Address address1 =
-        createAddress("HU", "test city 1", "test street 1", "test zipcode 1", "test houseNumber 1",
-            10, 20);
-    Milestone milestone1 = createMilestone(LocalDateTime.now().plusMinutes(30), address1);
-    Milestone milestone2 = createMilestone(LocalDateTime.now().plusMinutes(80), address1);
-    Milestone milestone3 = createMilestone(LocalDateTime.now().plusHours(2), address1);
-    Milestone milestone4 = createMilestone(LocalDateTime.now().plusHours(4), address1);
-    Section section1 = createSection(0, milestone1, milestone2);
-    Section section2 = createSection(1, milestone3, milestone4);
-    TransportPlan transportPlan = createTransportPlan(10000);
-    section1.setTransportPlan(transportPlan);
-    section2.setTransportPlan(transportPlan);
-    List<Section> sections = List.of(section1, section2);
-    transportPlan.setSections(sections);
+    LoginDto body = new LoginDto("Administrator", "pass");
+    jwt = webTestClient.post()
+        .uri("/api/login")
+        .bodyValue(body)
+        .exchange()
+        .expectBody(String.class)
+        .returnResult()
+        .getResponseBody();
   }
 
   @Test
-  public void testSetDelayWithStartMilestone() {
-    List<TransportPlan> transportPlansBefore = getAllTransportPlans();
-    TransportPlan transportPlanBefore = transportPlansBefore.get(0);
-    Section sectionBefore = transportPlanBefore.getSections().get(0);
+  public void testSetDelayWithMilestone() {
+    DelayDto delayDto = new DelayDto(1L, 30);
+    Milestone milestoneBefore = milestoneRepository.findById(delayDto.getMilestoneId()).get();
+    LocalDateTime plannedTimeBefore = milestoneBefore.getPlannedTime();
+    postDelay(1L, delayDto);
+    Milestone milestoneAfter = milestoneRepository.findById(delayDto.getMilestoneId()).get();
+    assertThat(milestoneAfter.getPlannedTime())
+        .isEqualTo(plannedTimeBefore.plusMinutes(delayDto.getDelay()));
+  }
+
+  @Test
+  public void testSetDelayWithStartMilestoneWorksCorrectly() {
+    TransportPlan transportPlanBefore = transportPlanRepository.findById(1L).get();
+    double incomeBefore = transportPlanBefore.getIncome();
+    Long sectionId = 1L;
+    Section sectionBefore = sectionRepository.findById(sectionId).get();
     Milestone fromBefore = sectionBefore.getFrom();
     Milestone toBefore = sectionBefore.getTo();
     LocalDateTime fromPlannedTimeBefore = fromBefore.getPlannedTime();
     LocalDateTime toPlannedTimeBefore = toBefore.getPlannedTime();
 
-    int delayInMinutes = 30;
+    int delayInMinutes = config.getReducing().getLimit1();
     DelayDto delayDto = new DelayDto(fromBefore.getId(), delayInMinutes);
 
-    postDelay(fromBefore.getId(), delayDto)
-        .expectStatus()
-        .isOk();
+    postDelay(transportPlanBefore.getId(), delayDto);
 
-    List<TransportPlan> transportPlansAfter= getAllTransportPlans();
-    TransportPlan transportPlanAfter = transportPlansAfter.get(0);
-    Section sectionAfter = transportPlanAfter.getSections().get(0);
+    TransportPlan transportPlanAfter = transportPlanRepository.findById(1L).get();
+    double incomeAfter = transportPlanAfter.getIncome();
+    Section sectionAfter = sectionRepository.findById(sectionId).get();
     Milestone fromAfter = sectionAfter.getFrom();
     Milestone toAfter = sectionAfter.getTo();
     LocalDateTime fromPlannedTimeAfter = fromAfter.getPlannedTime();
@@ -88,31 +95,32 @@ class TransportPlanControllerTest {
 
     assertThat(fromPlannedTimeBefore.plusMinutes(delayInMinutes)).isEqualTo(fromPlannedTimeAfter);
     assertThat(toPlannedTimeBefore.plusMinutes(delayInMinutes)).isEqualTo(toPlannedTimeAfter);
-    assertThat(transportPlanAfter.getIncome()).isEqualTo(9500);
+    assertThat(incomeAfter)
+        .isEqualTo(incomeBefore * ((double) (100 - config.getReducing().getPercent1()) / 100));
   }
 
   @Test
-  public void testSetDelayWithEndMilestone() {
-    List<TransportPlan> transportPlansBefore = getAllTransportPlans();
-    TransportPlan transportPlanBefore = transportPlansBefore.get(0);
-    Section section1Before = transportPlanBefore.getSections().get(0);
-    Section section2Before = transportPlanBefore.getSections().get(1);
+  public void testSetDelayWithEndMilestoneWorksCorrectly() {
+    TransportPlan transportPlanBefore = transportPlanRepository.findById(1L).get();
+    double incomeBefore = transportPlanBefore.getIncome();
+    Long sectionId1 = 1L;
+    Long sectionId2 = 2L;
+    Section section1Before = sectionRepository.findById(sectionId1).get();
+    Section section2Before = sectionRepository.findById(sectionId2).get();
     Milestone toBefore = section1Before.getTo();
     Milestone fromBefore = section2Before.getFrom();
     LocalDateTime toPlannedTimeBefore = toBefore.getPlannedTime();
     LocalDateTime fromPlannedTimeBefore = fromBefore.getPlannedTime();
 
-    int delayInMinutes = 60;
+    int delayInMinutes = config.getReducing().getLimit2();
     DelayDto delayDto = new DelayDto(2L, delayInMinutes);
 
-    postDelay(toBefore.getId(), delayDto)
-        .expectStatus()
-        .isOk();
+    postDelay(transportPlanBefore.getId(), delayDto);
 
-    List<TransportPlan> transportPlansAfter= getAllTransportPlans();
-    TransportPlan transportPlanAfter = transportPlansAfter.get(0);
-    Section section1After = transportPlanAfter.getSections().get(0);
-    Section section2After = transportPlanAfter.getSections().get(1);
+    TransportPlan transportPlanAfter = transportPlanRepository.findById(1L).get();
+    double incomeAfter = transportPlanAfter.getIncome();
+    Section section1After = sectionRepository.findById(sectionId1).get();
+    Section section2After = sectionRepository.findById(sectionId2).get();
     Milestone toAfter = section1After.getTo();
     Milestone fromAfter = section2After.getFrom();
     LocalDateTime toPlannedTimeAfter = toAfter.getPlannedTime();
@@ -120,65 +128,48 @@ class TransportPlanControllerTest {
 
     assertThat(toPlannedTimeBefore.plusMinutes(delayInMinutes)).isEqualTo(toPlannedTimeAfter);
     assertThat(fromPlannedTimeBefore.plusMinutes(delayInMinutes)).isEqualTo(fromPlannedTimeAfter);
-    assertThat(transportPlanAfter.getIncome()).isEqualTo(9000);
+    assertThat(incomeAfter)
+        .isEqualTo(incomeBefore * ((double) (100 - config.getReducing().getPercent2()) / 100));
   }
 
-  private List<TransportPlan> getAllTransportPlans() {
-    return webTestClient
-        .get()
-        .uri(BASE_URI)
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBodyList(TransportPlan.class)
-        .returnResult()
-        .getResponseBody();
-  }
-
-  private ResponseSpec postDelay(Long transportPlanId, DelayDto delayDto){
-    String path = BASE_URI + "/" + transportPlanId + "/delay";
-    return webTestClient
+  @Test
+  public void testThatMilestoneIdNotPartOfTransportPlanThrowBadRequest() {
+    String path = BASE_URI + "/" + 1 + "/delay";
+    DelayDto delayDto = new DelayDto(0L, 120);
+    webTestClient
         .post()
         .uri(path)
+        .headers(headers -> headers.setBearerAuth(jwt))
         .bodyValue(delayDto)
-        .exchange();
+        .exchange()
+        .expectStatus()
+        .isBadRequest();
   }
 
-  private Address createAddress(String ISO, String city, String street, String zipCode,
-                                String houseNumber, double width, double length) {
-    return addressRepository.save(
-        Address.builder()
-            .ISO(ISO)
-            .city(city)
-            .street(street)
-            .zipCode(zipCode)
-            .houseNumber(houseNumber)
-            .width(width)
-            .length(length)
-            .build());
+  @Test
+  public void testThatNonexistentTransportPlanThrowNotFound() {
+    String path = BASE_URI + "/" + 0 + "/delay";
+    DelayDto delayDto = new DelayDto(1L, 10);
+    webTestClient
+        .post()
+        .uri(path)
+        .headers(headers -> headers.setBearerAuth(jwt))
+        .bodyValue(delayDto)
+        .exchange()
+        .expectStatus()
+        .isNotFound();
   }
 
-  private Milestone createMilestone(LocalDateTime plannedTime, Address address) {
-    return milestoneRepository.save(
-        Milestone.builder()
-            .plannedTime(plannedTime)
-            .address(address)
-            .build());
+  private void postDelay(Long transportPlanId, DelayDto delayDto) {
+    String path = BASE_URI + "/" + transportPlanId + "/delay";
+    webTestClient
+        .post()
+        .uri(path)
+        .headers(headers -> headers.setBearerAuth(jwt))
+        .bodyValue(delayDto)
+        .exchange()
+        .expectStatus()
+        .isOk();
   }
 
-  private Section createSection(int number, Milestone from, Milestone to) {
-    return sectionRepository.save(
-        Section.builder()
-            .number(number)
-            .from(from)
-            .to(to)
-            .build());
-  }
-
-  private TransportPlan createTransportPlan(double income) {
-    return transportPlanRepository.save(
-        TransportPlan.builder()
-            .income(income)
-            .build());
-  }
 }
