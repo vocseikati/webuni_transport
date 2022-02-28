b24b330b356df29fefbc820c72f77eec1005de66
Katka <vocseikati@gmail.com>
2022-02-28 14:09:03 +0100

    deleteAddress fixed

--- src/main/java/hu/katka/transport/repositories/MilestoneRepository.java
+++ src/main/java/hu/katka/transport/repositories/MilestoneRepository.java
@@ -1,7 +1,10 @@
 package hu.katka.transport.repositories;
 
 import hu.katka.transport.entities.Milestone;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
+
+  List<Milestone> findByAddressId(Long addressId);
 }
--- src/main/java/hu/katka/transport/services/DefaultAddressService.java
+++ src/main/java/hu/katka/transport/services/DefaultAddressService.java
@@ -3,18 +3,19 @@ package hu.katka.transport.services;
 import static hu.katka.transport.services.ValidationService.requireNonNull;
 import static hu.katka.transport.services.ValidationService.requireNull;
 
-import hu.katka.transport.dtos.AddressDto;
 import hu.katka.transport.entities.Address;
+import hu.katka.transport.entities.Milestone;
 import hu.katka.transport.exceptions.EntityNotFoundException;
 import hu.katka.transport.repositories.AddressRepository;
+import hu.katka.transport.repositories.MilestoneRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 @Service
@@ -22,6 +23,9 @@ public class DefaultAddressService implements AddressService{
 
   @Autowired
   private AddressRepository addressRepository;
+
+  @Autowired
+  private MilestoneRepository milestoneRepository;
 
   @Override
   public List<Address> findAll() {
@@ -34,24 +38,29 @@ public class DefaultAddressService implements AddressService{
   }
 
   @Override
+  @Transactional
   public void deleteAddress(Long id) {
-    if (!addressRepository.existsById(id)){
-      return;
+    Address address = getAddressOrThrow(id);
+    List<Milestone> milestones = milestoneRepository.findByAddressId(address.getId());
+    for (Milestone milestone : milestones) {
+      milestone.setAddress(null);
     }
-    addressRepository.deleteById(id);
+    addressRepository.delete(address);
   }
 
   @Override
+  @Transactional
   public Address modifyAddress(Address address, Long id) {
     if (address.getId() != null && !address.getId().equals(id)){
       throw new IllegalArgumentException("Address id should be null or equal with the provided id.");
     }
-    Address originalAddress = getAddressOrThrow(id);
+    getAddressOrThrow(id);
     address.setId(id);
     return addressRepository.save(address);
   }
 
   @Override
+  @Transactional
   public Address addAddress(Address newAddress) {
     requireNull(newAddress.getId(), "Address id must be null.");
     requireNonNull(newAddress, "Address must not be null.");
